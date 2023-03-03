0a9fdff70aae1d946b9499ecd03245d6d0b28f5c
Dmitriy Sapozhnikov <aurariel@gmail.com>
2023-03-03 18:27:41 +0300

    project barebone

---
+++ frontend/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "App starts here" << std::endl;
+    return 0;
+}
---
+++ tests/src/test1.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(DummySuite, DummySuite__Test)
+{
+    ASSERT_EQ(1, 1);
+}
